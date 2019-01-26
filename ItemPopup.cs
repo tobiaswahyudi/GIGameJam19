5b353a40127c770ab32578966fddadd558d955b6
CosmicWolfe <43656526+CosmicWolfe@users.noreply.github.com>
2019-01-26 18:37:50 -0500

    Add files via upload

---
+++ ItemPopup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPopup : MonoBehaviour
+{
+
+    public Text popupText;
+
+    public Animator animate;
+
+    private Queue<string> words;
+
+    private float timing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        words = new Queue<string>();
+        
+    }
+
+    public void ShowDialogue(Dialogue texts)
+    {
+        timing = Time.time;
+
+        animate.SetBool("isOpen", true);
+
+        words.Clear();
+
+        foreach (string sentence in texts.sentences)
+        {
+            words.Enqueue(sentence);
+        }
+
+        popupText.text = words.Dequeue();
+
+
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (animate.GetBool("isOpen")) {
+            if ( (Time.time - timing)> 1.5) {
+                animate.SetBool("isOpen", false);
+            }
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//private Collider2D MainCol,JumpCol;
+	private Rigidbody2D RB;
+	
+	public float MaxSpeed,JumpForce;
+	public float JumpDelay;
+	public float JumpElapsed;
+    public bool notInDialogue;
+
+	private int touching;
+
+	// Use this for initialization
+	void Start () {
+        notInDialogue = true;
+		JumpElapsed = 0;
+		touching = 0;
+		//MainCol = GetComponents<Collider2D>()[0];
+		//JumpCol = GetComponents<Collider2D>()[0];
+		RB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (notInDialogue) 
+        {
+		JumpElapsed += Time.deltaTime;
+		JumpElapsed = Mathf.Clamp(JumpElapsed, 0, 2);
+		float hzin = Input.GetAxis("Horizontal");
+		this.transform.Translate(new Vector3(MaxSpeed*hzin, 0.0f, 0.0f));
+		if (touching > 0 && JumpElapsed > JumpDelay && Input.GetAxis("Jump") > 0.5) {
+			RB.AddForce(new Vector2(0.0f, JumpForce));
+			JumpElapsed = 0;
+            }
+		}
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		touching++;
+		Debug.Log(touching);
+	}
+	private void OnTriggerExit2D(Collider2D collision) {
+		touching--;
+		Debug.Log(touching);
+	}
+}
