fdc7f7ff4a359481df5dc0a947178c655b9ed839
CosmicWolfe <43656526+CosmicWolfe@users.noreply.github.com>
2019-01-26 14:16:44 -0500

    Add files via upload

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform Player;
+	public float CamSpeed;
+	public float CamHeight;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Vector3.Lerp(this.transform.position,Player.transform.position + new Vector3(0 ,CamHeight, -10),
+			CamSpeed*(Mathf.Abs(Vector3.Distance(this.transform.position, Player.transform.position))-8.0f));
+	}
+}
---
+++ Scripts/SpriteDialogueTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpriteDialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject adventurer;
+    public GameObject npc;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Renderer>().enabled = false;
+        Debug.Log("Test");
+    }
+
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+
+    private void OnMouseDown()
+    {
+        if (this.GetComponent<Renderer>().enabled)
+        {
+            Debug.Log("yoo");
+            TriggerDialogue();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(npc.transform.position, adventurer.transform.position) <= 2)
+        {
+            this.GetComponent<Renderer>().enabled = true;
+        } else {
+            this.GetComponent<Renderer>().enabled = false;
+        }
+
+
+    }
+}
