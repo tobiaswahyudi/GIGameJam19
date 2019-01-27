66a1bedbfad41dfae7788e437460f74caadb6066
CosmicWolfe <43656526+CosmicWolfe@users.noreply.github.com>
2019-01-27 14:51:07 -0500

    Add files via upload

---
+++ ACurve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ACurve : MonoBehaviour
+{
+    public AnimationCurve ACx;
+    public AnimationCurve ACy;
+    public float periodX, periodY, amplitudeX, amplitudeY;
+    private float elapsedTimeX, elapsedTimeY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTimeX += Time.deltaTime;
+        elapsedTimeY += Time.deltaTime;
+        if (elapsedTimeX > periodX) elapsedTimeX -= periodX;
+        if (elapsedTimeY > periodY) elapsedTimeY -= periodY;
+        this.transform.position = new Vector3(ACx.Evaluate(elapsedTimeX / periodX) * amplitudeX - 10, ACy.Evaluate(elapsedTimeY/periodY) * amplitudeY + 3.6f, 0.0f);
+    }
+}
